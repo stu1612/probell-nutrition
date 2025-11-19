0026f2755b3d4d9e72749e83bfefdbe24721f170
Stu Bolderson <stu.bolderson@aol.com>
2025-11-19 14:22:59 +0100

    fix: remove Navbar logic as the CMS logic is no longer used to prevent type error on build

--- src/app/components/nav/Navbar.tsx
+++ src/app/components/nav/Navbar.tsx
@@ -1,26 +1,30 @@
-// internal libs (api, queries, uitls, enums, types)
-import { hygraph } from "@/lib/hygraph";
-import { NAVBAR_BLOCK } from "@/lib/queries";
-import { CMS_NavItem } from "./types";
-import { toNavVM } from "./mappers";
+// import { hygraph } from "@/lib/hygraph";
+// import { NAVBAR_BLOCK } from "@/lib/queries";
+// import { CMS_NavItem } from "./types";
+// import { toNavVM } from "./mappers";
 
-// compoenents
-import NavbarClient from "./NavbarClient";
+// import NavbarClient from "./NavbarClient";
 
-type CMS_Navbar = {
-  id: string;
-  navItems: CMS_NavItem[];
-};
+// type CMS_Navbar = {
+//   id: string;
+//   navItems: CMS_NavItem[];
+// };
 
-export default async function Navbar() {
-  const { navbars } = await hygraph<{ navbars: CMS_Navbar[] }>({
-    query: NAVBAR_BLOCK,
-    variables: { stage: "PUBLISHED" },
-  });
+// export default async function Navbar() {
+//   const { navbars } = await hygraph<{ navbars: CMS_Navbar[] }>({
+//     query: NAVBAR_BLOCK,
+//     variables: { stage: "PUBLISHED" },
+//   });
 
-  // properties
-  const navbar = navbars?.[0];
-  const vm = toNavVM(navbar?.navItems ?? []);
+//   // properties
+//   const navbar = navbars?.[0];
+//   const vm = toNavVM(navbar?.navItems ?? []);
 
-  return <NavbarClient vm={vm} />;
+//   return <NavbarClient vm={vm} />;
+// }
+
+import React from "react";
+
+export default function Navbar() {
+  return <div>Navbar</div>;
 }
