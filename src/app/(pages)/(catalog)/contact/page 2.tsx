f610da76c99cfbebe23fac9cc9cdc5a1a9b53206
Stu Bolderson <stu.bolderson@aol.com>
2025-11-27 19:06:14 +0100

    clean - remove static images and duplicated files (#20)

---
+++ src/app/(pages)/(catalog)/contact/page 2.tsx
@@ -0,0 +1,5 @@
+import UnderConstruction from "@/app/components/globals/UnderConstruction";
+
+export default function Contact() {
+  return <UnderConstruction />;
+}
---
+++ src/app/(pages)/(catalog)/the-kettlebell/page 2.tsx
@@ -0,0 +1,5 @@
+import UnderConstruction from "@/app/components/globals/UnderConstruction";
+
+export default function Kettlebell() {
+  return <UnderConstruction />;
+}
